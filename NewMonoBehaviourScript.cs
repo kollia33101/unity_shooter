aa3b0658755ebdd124ed2a44c1dd6b47525df672
kollia33101 <kolapetrenko659@gmail.com>
2025-08-16 14:33:25 +0300

    изменили оружие

--- NewMonoBehaviourScript.cs
+++ NewMonoBehaviourScript.cs
@@ -7,6 +7,8 @@ public float lookSpeed = 2f; // Скорость поворота мыши
 public Camera playerCamera; // Камера для отображения от первого лица
 
 public bool onground = false;
+public GameObject rifle;
+public GameObject pistol;
 
 private float rotationX = 0f; // Угол поворота по X
 void OnCollisionEnter(Collision other){ onground = true;
@@ -30,10 +32,24 @@ void Update()
 Move();
 RotateView();
 shoot();
+ChangeWeapons();
 }
 void shoot(){
 	if (Input.GetKey(KeyCode.E)){Transform animator = transform.GetChild(0).GetChild(0);
 animator.GetComponent<Animation>().Play("New Animation");}
+}
+void ChangeWeapons(){
+	if(Input.GetKey(KeyCode.V)){//if (Input.GetKey(KeyCode.WheelUp)||Input.GetKey(KeyCode.WheelDown)){
+	Debug.Log("yes");
+	if (rifle.activeSelf){
+		pistol.SetActive(true);
+		rifle.SetActive(false); 
+    
+ 	} else {
+ 		pistol.SetActive(false);
+ 		rifle.SetActive(true);
+ 	}
+	}
 }
 void Move()
 {
